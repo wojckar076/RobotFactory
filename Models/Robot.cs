22891f441f6d95c5e1738eed8218cd5482ffecba
wojckar076 <zeusolimpupan@o2.pl>
2020-06-17 00:55:54 +0200

    last name fix

---
+++ Models/Robot.cs
@@ -0,0 +1,25 @@
+﻿using RobotFactory.Interfaces;
+
+namespace RobotFactory.Models
+{
+    public class Robot : IRobot
+    {
+        public string SerieNumber { get; set; }
+        public ProfessionType Profession { get; }
+
+        public Robot(string serieNumber, ProfessionType purpose)
+        {
+            SerieNumber = serieNumber;
+            Profession = purpose;
+        }
+        public void Reset(string newNumber)
+        {
+            SerieNumber = newNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{SerieNumber}, {Profession}";
+        }
+    }
+}
